363fe7fa48042bb6534ed44d191b715a6da5233e
adidjalalbekov-jpg <adidjalalbekov@gmail.com>
2026-02-26 11:12:24 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,95 @@
+// let hello = "Hello Work";
+// console.log(hello);
+// let name1  = "Chort";
+// let name2 = "Chikatila";
+// console. log(name1 + " менен " + name2 + " достор");
+
+// let a = 25;
+// let b = 30;
+// let sum = a + b
+// console.log(" Сумма " + sum);
+
+
+// let name1 = "Adil";
+// let age = 13;
+// let hobbies = " footbol, it,"
+// let mySchool = " №74 "
+// console.log("Name: " + name1 + ",Age:" + age + " ,Hobbies:" + hobbies + " ,MySchool:" + MySchool);
+   
+// let a = 2;
+// let b = 3;
+// let sum = a + b;
+// let difference = a - b;
+// let product = a * b;
+// let product1 = a ** b;
+// let quotient = a / b;
+// console.log(sum);
+// console.log(difference);
+// console.log(product);
+// console.log(quotient);
+
+
+ // String
+// let name = "Azat"
+// console.log(name);
+
+
+//  Number
+// let age = 9;
+// console.log(age);
+
+
+//  Boolean true or false
+// let isStudent = true ;
+// console.log(isStudent);
+
+//  Null
+// let apple = null;
+// console.log(apple);
+
+
+// Undefined
+// let banana;
+// console.log(banana);
+
+// let name = "Adil";
+// console.log(name);
+// console.log(typeof name);
+
+// let age = 9;
+// console.log(age);
+// console.log(typeof age);
+
+
+// let isStudent = true
+// console.log(typeof isStudent);
+
+
+//  alert("Hello, i am groot");
+
+
+//  let name = prompt ("What is your name?");
+//  let name = prompt("age?");
+//  console.log(name);
+// console.log(age);
+ 
+// let isStudent = confirm("Are you a Student?");
+// console.log(isStudent);
+
+// let city = "Bishkek";
+// console.log("Я живу в " + city);
+
+// let price = 150;a
+// console.log(price);
+// console.log(typeof price);
+
+// let isStudent = confirm("Are you a student?")
+// console.log(isStudent);
+
+
+
+// let name = prompt("What is your name?")
+// console.log("Hello " + name);
+
+
+
